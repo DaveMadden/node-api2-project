79d717a8b4f938ca9415877cefe64d395900b8a4
Dave Madden <davelearnsstuff@gmail.com>
2021-11-30 14:26:27 -0800

    update is working but returns a '1'

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -19,10 +19,56 @@ router.get('/', (req,res)=>{
 })
 
 //GET POST BY (ID)
+router.get('/:id', async (req,res) => {
+    try {
+        const blogpost = await Post.findById(req.params.id)
+        if (!blogpost){
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            res.status(200).json(blogpost);
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The post information could not be retrieved"
+        })
+    }
+})
 
 //CREATE NEW POST
+router.post('/', (req,res)=>{
+
+    if (!req.body.title || !req.body.contents){
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    } else {
+        Post.insert(req.body)
+            .then(blogpost => {
+                res.status(201).json(blogpost);
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "There was an error while saving the post to the database"
+                })
+            })
+    }
+})
 
 //UPDATE POST BY (ID)
+router.put('/:id', async (req,res)=>{
+    try {
+        if(!req.body.title || !req.body.contents){
+            res.status(400).json({message: "Please provide title and contents for the post"})
+        } else {
+            const updatedPost = await Post.update(req.params.id, req.body)
+            res.status(201).json(updatedPost)
+        }
+    } catch (error) {
+        res.status(500).json({ message: "The post with the specified ID does not exist" })
+    }
+})//this returns '1'?
 
 //DELETE POST BY (ID)
 
