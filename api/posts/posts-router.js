967deec55726e5351a942473d716c896a7bc8cbd
Dave Madden <davelearnsstuff@gmail.com>
2021-11-30 13:26:22 -0800

    get all working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,32 @@
 // implement your posts router here
+const express = require('express')
+const Post = require('./posts-model')
+const router = express.Router()
+
+//GET ALL POSTS
+router.get('/', (req,res)=>{
+    Post.find()
+        .then(posts => {
+            // throw new Error("REKT");
+            res.status(200).json(posts);
+        })
+        .catch(error=>{
+            console.log(error);
+            res.status(500).json({
+                message: "The posts information could not be retrieved",
+            });
+        })
+})
+
+//GET POST BY (ID)
+
+//CREATE NEW POST
+
+//UPDATE POST BY (ID)
+
+//DELETE POST BY (ID)
+
+//GET ALL COMMENTS BY (ID)
+
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,23 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express')
+const server = express();
+
+const postRouter = require('./posts/posts-router')
+
+server.use(express.json());
+server.use('/api/posts', postRouter)
+
+
+
+
+
+//default endpoint
+server.get('/', (req, res) => {
+    res.send(`
+      <h2>BLOG_API</h>
+      <p>Welcome to the sick blog API bro</p>
+    `);
+  });
+
+module.exports = server;
